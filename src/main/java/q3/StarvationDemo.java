1489e64a4f8b1a75c3b2951b8c18982ab98b3d48
mamad <you@example.com>
2024-02-22 05:10:32 +0330

    add run method to StarvationDemo class

--- src/main/java/q3/StarvationDemo.java
+++ src/main/java/q3/StarvationDemo.java
@@ -1,4 +1,10 @@
 package q3;
 
 public class StarvationDemo extends Thread{
+
+    public void run() {
+        threadcount++;
+        System.out.println(threadcount + "st Child Thread execution starts");
+        System.out.println("Child thread execution completes");
+    }
 }
