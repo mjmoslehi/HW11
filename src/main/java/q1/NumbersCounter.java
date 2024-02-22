8abcd57923749f569e48f9bd64bf9799b86148df
mamad <you@example.com>
2024-02-22 05:06:37 +0330

    add ArrayList objects to NumbersCounter class

--- src/main/java/q1/NumbersCounter.java
+++ src/main/java/q1/NumbersCounter.java
@@ -1,8 +1,16 @@
 package q1;
 
+import java.util.ArrayList;
+
 public class NumbersCounter {
+
     int number;
+
     public NumbersCounter(int number) {
         this.number = number;
     }
+
+    ArrayList<Integer> odd = new ArrayList<>();
+    ArrayList<Integer> even = new ArrayList<>();
+    ArrayList<Integer> sum = new ArrayList<>();
 }
