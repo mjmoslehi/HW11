0554e40d1147b3c39b8c6d71615872a22fc892a4
mamad <you@example.com>
2024-02-22 05:06:58 +0330

    add countEven method to NumbersCounter class

--- src/main/java/q1/NumbersCounter.java
+++ src/main/java/q1/NumbersCounter.java
@@ -13,4 +13,25 @@ public class NumbersCounter {
     ArrayList<Integer> odd = new ArrayList<>();
     ArrayList<Integer> even = new ArrayList<>();
     ArrayList<Integer> sum = new ArrayList<>();
+
+    public synchronized void countEven() {
+
+        try {
+
+            for (int i = 0; i <= number; i++) {
+                if (i % 2 == 0) {
+                    even.add(i);
+                }
+            }
+
+            System.out.println("even numbers :");
+            System.out.println(even);
+
+
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
