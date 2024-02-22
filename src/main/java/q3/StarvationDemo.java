090388a261cd4232513a09e9375211c9f347134a
mamad <you@example.com>
2024-02-22 05:10:51 +0330

    add field to StarvationDemo class

--- src/main/java/q3/StarvationDemo.java
+++ src/main/java/q3/StarvationDemo.java
@@ -1,7 +1,7 @@
 package q3;
 
 public class StarvationDemo extends Thread{
-
+    static int threadcount = 0;
     public void run() {
         threadcount++;
         System.out.println(threadcount + "st Child Thread execution starts");
